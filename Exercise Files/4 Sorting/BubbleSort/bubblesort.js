65b3163a5692fa1af6bea43dead675cf28a9be79
LisaCee <lisaceedesign@gmail.com>
2019-04-23 16:45:44 -0700

    removed comments

--- Exercise Files/4 Sorting/BubbleSort/bubblesort.js
+++ Exercise Files/4 Sorting/BubbleSort/bubblesort.js
@@ -1,8 +1,6 @@
 const bubbleSort = dataset => {
     for (let i = dataset.length - 1; i >= 0; i--) {
-        // i = 9; i >= 0; i--
         for (let j = i; j >= 0; j--) {
-            // j = 9; j < 
             if (dataset[j] > dataset[j + 1]) {
                 let temp = dataset[j];
                 dataset[j] = dataset[j + 1];
