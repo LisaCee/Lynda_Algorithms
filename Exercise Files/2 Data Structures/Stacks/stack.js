58b3919f1f6a53b729aeabbb28413e2c08975580
LisaCee <lisaceedesign@gmail.com>
2019-04-28 16:29:33 -0700

    stack.js

---
+++ Exercise Files/2 Data Structures/Stacks/stack.js
@@ -0,0 +1,15 @@
+// TODO: create a new empty stack
+let stack = [];
+
+// TODO: push items onto the stack
+stack.push('item1');
+stack.push('item2');
+stack.push('item3');
+
+// TODO: print the stack contents
+console.log(stack);
+
+// TODO: pop an item off the stack
+let popped = stack.pop();
+console.log(popped);
+console.log(stack);
