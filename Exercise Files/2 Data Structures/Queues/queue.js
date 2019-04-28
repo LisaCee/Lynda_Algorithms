2cf83bac21ec29f87732c348a72f1a21d0570229
LisaCee <lisaceedesign@gmail.com>
2019-04-28 16:32:36 -0700

    queue.js

---
+++ Exercise Files/2 Data Structures/Queues/queue.js
@@ -0,0 +1,16 @@
+// TODO: create a new empty deque object that will function as a queue
+let queue = []
+
+// TODO: add some items to the queue
+queue.push('a');
+queue.push('b');
+queue.push('c');
+queue.push('d');
+
+// TODO: print the queue contents
+console.log(queue);
+
+// TODO: pop an item off the front of the queue
+let popped = queue.shift();
+console.log(popped);
+console.log(queue);
